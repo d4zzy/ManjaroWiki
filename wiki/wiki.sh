a4fb3cf83b16648bc05ac7b3d43955f26cfc3ffc
dazzay <dazzay@users.noreply.github.com>
2015-06-19 21:17:47 +0100

    Update wiki.sh

--- wiki/wiki.sh
+++ wiki/wiki.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 if [ "--help" == "$1" ]; then
-	this=`basename $0`
+	this=${0##*/}
 	echo
-	echo "Usage: $this archwiki page name"
+	echo "Usage: $this manjarowiki page name"
 	echo
-	echo "  where archwiki page name is title of page on wiki.archlinux.org"
+	echo "  where manjaro page name is title of page on wiki.manjaro.org"
 	echo
 	echo "Examples:"
 	echo "  $this ssh"
-	echo "  $this the arch way"
+	echo "  $this the manjaro way"
 	echo "  $this beginners guide"
 	echo
 	exit 0
 fi
 
 # try to detect a console browser:
-if   [ -x /usr/bin/lynx ];   then run_browser=/usr/bin/lynx  # Lynx first because of the pretty color output
-elif [ -x /usr/bin/elinks ]; then run_browser=/usr/bin/elinks  # Elinks second because it newer fork of original Links
-elif [ -x /usr/bin/links ];  then run_browser=/usr/bin/links  # If anyone uses...
+if [ -n "$BROWSER" ];        then run_browser=$BROWSER        # Some users might have set the $BOWSER variable
+elif [ -x $(which lynx) ];   then run_browser=$(which lynx)   # Lynx first because of the pretty color output
+elif [ -x $(which elinks) ]; then run_browser=$(which elinks) # Elinks second because it newer fork of original Links
+elif [ -x $(which links) ];  then run_browser=$(which links)  # If anyone uses...
 
 else  # no console browser found -> exit
 	echo "Please install one of the following packages to use this script: elinks links lynx"
@@ -29,7 +30,9 @@ fi
 query="$*"  # get all params into single query string
 query=${query// /_}  # substitute spaces with underscores in the query string
 
-# load ArchWiki page with automatic redirect to the correct URL:
+# load ManjaroWiki page with automatic redirect to the correct URL:
+
+# load ManjaroWiki page with automatic redirect to the correct URL:
 "$run_browser" "https://wiki.manjaro.org/index.php/Special:Search/${query}"
 
 exit $?  # return browser's exit code
